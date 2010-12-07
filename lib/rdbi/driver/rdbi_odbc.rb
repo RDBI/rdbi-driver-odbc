4982345a218c479d00aaaf6607085f7a0a7dbba6
Shane Emmons <semmons99@gmail.com>
2010-12-07 10:33:00 -0500

    initial code for Cursor and Statement

--- lib/rdbi/driver/rdbi_odbc.rb
+++ lib/rdbi/driver/rdbi_odbc.rb
@@ -37,7 +37,7 @@ class RDBI::Driver::ODBC < RDBI::Driver
     end
 
     def transaction(&block)
-      raise NotImplementedError, "not supported"
+      raise NotImplementedError, "#transaction"
     end
 
     def rollback
@@ -55,11 +55,11 @@ class RDBI::Driver::ODBC < RDBI::Driver
     end
 
     def table_schema(table_name)
-      raise NotImplementedError, "not supported"
+      raise NotImplementedError, "#table_schema"
     end
 
     def schema
-      raise NotImplementedError, "not supported"
+      raise NotImplementedError, "#schema"
     end
 
     def ping
@@ -68,8 +68,163 @@ class RDBI::Driver::ODBC < RDBI::Driver
   end
 
   class Cursor < RDBI::Cursor
+
+    def initialize(handle)
+      super handle
+      @index = 0
+    end
+
+    def next_row
+      val = if @array_handle
+              @array_handle[@index]
+            else
+              @handle.fetch
+            end
+      @index += 1
+      val
+    end
+
+    def result_count
+      if @array_handle
+        @array_handle.size
+      else
+        @handle.nrows
+      end
+    end
+
+    def affected_count
+      if @array_handle
+        0
+      else
+        @handle.nrows
+      end
+    end
+
+    def first
+      if @array_handle
+        @array_handle.first
+      else
+        @handle.fetch_first
+      end
+    end
+
+    def last
+      if @array_handle
+        @array_handle.last
+      else
+        @handle.fetch_scroll(::ODBC::SQL_FETCH_LAST)
+      end
+    end
+
+    def rest
+      if @array_handle
+        @array_handle[@index+1..-1]
+      else
+        @handle.fetch_all[1..-1]
+      end
+    end
+
+    def all
+      if @array_handle
+        @array_handle
+      else
+        @handle.fetch_scroll(::ODBC::SQL_FETCH_FIRST) +
+          @handle.fetch_all
+      end
+    end
+
+    def fetch(count = 1)
+      return [] if last_row?
+      if @array_handle
+        @array_handle[@index, count]
+      else
+        @handle.fetch_many(count)
+      end
+    end
+
+    def [](index)
+      if @array_handle
+        @array_handle[index]
+      else
+        @handle.fetch_scroll(::ODBC::SQL_FETCH_ABSOLUTE, index)
+      end
+    end
+
+    def last_row?
+      if @array_handle
+        @index == @array_handle.size
+      else
+        @index == @handle.nrows
+      end
+    end
+
+    def empty?
+      if @array_handle
+        @array_handle.empty?
+      else
+        @handle.nrows == 0
+      end
+    end
+
+    def rewind
+      return if @index == 0
+      @index = 0
+      unless @array_handle
+        @handle.fetch_first
+      end
+    end
+
+    def size
+      if @array_handle
+        @array_handle.length
+      else
+        @handle.nrows
+      end
+    end
+
+    def finish
+      @handle.drop
+    end
+
+    def coerce_to_array
+      unless @array_handle
+        @array_handle = []
+        begin
+          @array_handle << @handle.fetch_first
+          @array_handle += @handle.fetch_all
+        rescue
+        end
+      end
+    end
   end
 
   class Statement < RDBI::Statement
+    extend MethLab
+
+    attr_accessor :handle
+
+    def initialize(query, dbh)
+      super
+
+      ep = Epoxy.new(query)
+      @handle = @dbh.handle.prepare(ep.query)
+      @output_type_map = RDBI::Type.create_type_hash(RDBI::Type::Out)
+    end
+
+    def new_execution(*binds)
+      @handle.execute(*binds)
+
+      columns = @handle.columns(true).collect do |col|
+        newcol = RDBI::Column.new
+        newcol.name = col.name.to_sym
+        newcol
+      end
+
+      return Cursor.new(@handle), RDBI::Schema.new(columns), @output_type_map
+    end
+
+    def finish
+      @handle.drop
+    end
   end
 end
